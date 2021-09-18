d5d141156c918f89383fb3687fb37bf8125a132f
Maximilian Rice <maximilian.rice1@gmail.com>
2021-09-18 08:25:04 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.get('/greeting/:name', (req, res) => {
     res.send("Hello, " + req.params.name + "! Great to see you!")
 })
 
+// /tip route
+// app.get('/tip/:total/:tipPercentage', (req, res) => {
+//     const total = +req.params.total
+//     const tipPerc = +req.params.tipPercentage
+//     res.send(total + tipPerc)
+// })
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {   
+    res.send("The tip is $" + ((+req.params.tipPercentage)*.01* (+req.params.total)).toFixed(2));
+});
+
 app.listen(port, () =>{
     console.log(`this is port ${port}`)
 })
